2b9fdd34bd29393d5a32d85904a529162addd3a8
root <root@477a1112f5d6>
2020-04-04 01:30:01 +0000

    commit commit

--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -33,14 +33,13 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//3.  Ask the user to enter a positive number and convert it to an int
 	String number =	JOptionPane.showInputDialog(null, "Give me a number");
 	int num = Integer.parseInt(number);
-	button = new JButton[num];
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-	
+	button = new JButton[num];
 		//5. Make a for loop to iterate through the JButton array
 	for (int i = 0; i < num; i++) {
 		button[i] = new JButton();
 		button[i].addActionListener(this);
-		button[i].add(panel);
+		panel.add(button[i]);
 	}
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
@@ -53,7 +52,7 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//11. set the JFrame to visible.
 		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		JOptionPane.showMessageDialog(null, "idk yet");
+		JOptionPane.showMessageDialog(null, "Try to find random button");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
 		Random rand = new Random();
 		hiddenButton = rand.nextInt(num);
@@ -75,13 +74,13 @@ button[hiddenButton].setText("");
 		JButton buttonClicked = (JButton)e.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
-if (buttonClicked == button[i]) {
+if (buttonClicked == button[hiddenButton]) {
 	JOptionPane.showMessageDialog(null, "You won");
-	
+}
 	else {
-		JOptionPane.showMessageDialog(null, "Try again")
+		JOptionPane.showMessageDialog(null, "Try again");
 	}
 }
 		//18. else tell them to try again
 	}
-}
+
